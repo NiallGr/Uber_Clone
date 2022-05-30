074c8dfcf82111c1f985ac353de1c4c4873bca10
Niall Grainger <niallgrainger@gmail.com>
2022-05-29 21:23:16 -0700

    Adding Navigation

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,9 @@
 import { FlatList, TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
 import tw from "tailwind-react-native-classnames";
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+
 
 
 
@@ -24,6 +27,9 @@ const data = [
 
 
 const NavOptions = () => {
+
+const navigation = useNavigation();
+
   return (
     <FlatList
         data={data}
@@ -31,7 +37,8 @@ const NavOptions = () => {
         horizontal
         renderItem={({ item }) => (
             <TouchableOpacity
-            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+            onPress={() => navigation.navigate(item.screen)}
+            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-1 w-30`}>
             <View>
                 <Image
                     style={{ width: 120, height: 120, resizeMode: 'contain' }}
@@ -40,6 +47,9 @@ const NavOptions = () => {
                 <Text style={tw`mt-2 text-lg font-semibold`}>
                     {item.title}
                 </Text>
+                <Icon 
+                style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                name="arrowright" color="white" type="antdesign"></Icon>
             </View>
             </TouchableOpacity>
         )}
