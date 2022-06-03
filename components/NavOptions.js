ceee2b45111783eb9a60a8c211c7286f192d5f67
Niall Grainger <niallgrainger@gmail.com>
2022-06-03 13:17:00 -0700

    Updated funtionality of map,search & pin loc

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,6 +3,8 @@ import React from 'react';
 import tw from "tailwind-react-native-classnames";
 import { Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
 
 
@@ -29,6 +31,7 @@ const data = [
 const NavOptions = () => {
 
 const navigation = useNavigation();
+const origin = useSelector(selectOrigin);
 
   return (
     <FlatList
@@ -38,8 +41,11 @@ const navigation = useNavigation();
         renderItem={({ item }) => (
             <TouchableOpacity
             onPress={() => navigation.navigate(item.screen)}
-            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-1 w-30`}>
-            <View>
+            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-1 w-30`}
+            disabled={!origin}
+            >
+            
+            <View style={tw`${!origin && 'opacity-20'}`}>
                 <Image
                     style={{ width: 120, height: 120, resizeMode: 'contain' }}
                     source={{ uri: item.image }}
