fad5d98eb0c1b53d2ead0178945a2e4e947835f1
Niall Grainger <niallgrainger@gmail.com>
2022-06-03 13:26:26 -0700

    Added Google Search Option NavageCard

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,11 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'tailwind-react-native-classnames'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
 const NavigateCard = () => {
   return (
-    <View>
-      <Text>Yo</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, Niall!</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+            <View>
+                <GooglePlacesAutocomplete placeholder='Where to?'/>
+            </View>
+
+      </View>
+    </SafeAreaView>
   )
 }
 
