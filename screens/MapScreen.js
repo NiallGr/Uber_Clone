0b5a618d4a2a799f834410754b3ea35d9e5ba0f4
Niall Grainger <niallgrainger@gmail.com>
2022-07-04 21:19:14 -0700

    Add Navagation button

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
@@ -6,12 +6,21 @@ import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
 import MapView from 'react-native-maps';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
   return (
     <View>
+      
+    <TouchableOpacity
+    onPress={() => navigation.navigate("HomeScreen")}
+    style={tw`bg-gray-100 z-50 absolute left-4 top-16 p-3 rounded-full shadow-lg`}>
+      <Icon name="menu" />
+
+    </TouchableOpacity>
 
       <View style={tw`h-2/5`}>
         <Map/>
