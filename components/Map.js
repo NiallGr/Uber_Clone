3597fd9c53e2b39bd929eed23d5a597e5da9540f
Niall Grainger <niallgrainger@gmail.com>
2022-07-04 21:16:45 -0700

    Added Navigation button

--- components/Map.js
+++ components/Map.js
@@ -21,7 +21,7 @@ const Map = () => {
       mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
         edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
       });
-  }, [origin, destination]);
+  });
 
  useEffect(() => {
 
